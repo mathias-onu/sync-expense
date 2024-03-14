3d35fd4cfa853e6140513868d0fee423a213245d
mathias-onu <mathias.onu@outlook.com>
2024-03-14 13:03:39 +0200

    added cors config

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import express from "express"
 import dotenv from 'dotenv'
 import router from "./server/routes/routes.js"
+import cors, { CorsOptions } from "cors"
 
 const app = express()
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+}
 
 dotenv.config()
 
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use("/api", router)
 
 app.listen(process.env.PORT, () => {
